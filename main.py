d61ff5f0de1fa02418a1a252b46881002b6d27ac
Krzysztof Nawrocki <k.nawrocki@hotmail.com>
2022-05-30 21:41:13 +0200

    final

--- main.py
+++ main.py
@@ -2,5 +2,4 @@
 
 
 class HerokuApp:
-    # app_url = ""https://djakdeploymentkn.herokuapp.com/""  # Fill your heroku app url here
-    app_url = "http://127.0.0.1:8000"
+    app_url = ""https://djakdeploymentkn.herokuapp.com/""  # Fill your heroku app url here
